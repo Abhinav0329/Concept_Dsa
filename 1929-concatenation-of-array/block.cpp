91d960beab4335703b41b0be102ac9f1dc348f3f
Abhinav <abhinavupadhyay0329@gmail.com>
2026-07-02 00:24:33 +0530

    refactor: remove the trailing comma

--- 1929-concatenation-of-array/block.cpp
+++ 1929-concatenation-of-array/block.cpp
@@ -39,19 +39,24 @@ int size;
     cin >> size;
     
     vector<int> input(size);
-    cout << "Enter " << size << " elements: ";
+    cout << "Enter the elements: ";
     for(int i = 0; i < size; i++){
         cin >> input[i];
     }
+
     int k;
     cout << "Enter the value of k: ";
     cin >> k;
+
     vector<int> result = solver.multiply(input, k);
-    cout << "Result :[ ";
+    int count=0;
+    cout << "Result :[";
     for (int num : result){
-        cout << num << " ";
+        cout << num;
+        if(count++ < result.size()-1){
+            cout << ",";
+        }
     }
     cout << "]" << endl;
-    
     return 0;
 }
